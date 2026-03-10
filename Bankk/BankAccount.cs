f71cd33cd1d8281bf1f4512bc7b94ca27d8b40b9
234922 <234922@KB37-120-C15>
2026-03-10 14:00:15 +0300

    XML-комментарии

--- Bankk/BankAccount.cs
+++ Bankk/BankAccount.cs
@@ -4,29 +4,59 @@ namespace BankAccountNS
 {
     /// <summary>
     /// Bank account demo class.
+    /// Позволяет выполнять операции снятия и зачисления средств
     /// </summary>
     public class BankAccount
     {
+        /// <summary>
+        /// Имя владельца света
+        /// </summary>
         private readonly string m_customerName;
+        /// <summary>
+        /// Текущий баланс счета
+        /// </summary>
         private double m_balance;
+        /// <summary>
+        /// Закрытый конструктор по умолчанию
+        /// </summary>
 
         private BankAccount() { }
+        /// <summary>
+        /// Создает новый банковский счет
+        /// </summary>
+        /// <param name="customerName">Имя клиента</param>
+        /// <param name="balance">Начальный баланс счета</param>
 
         public BankAccount(string customerName, double balance)
         {
             m_customerName = customerName;
             m_balance = balance;
         }
+        /// <summary>
+        /// Получает имя владельца счета
+        /// </summary>
+        /// <value>Имя клиента</value>
 
         public string CustomerName
         {
             get { return m_customerName; }
         }
+        /// <summary>
+        /// Получает текущий баланс счета
+        /// </summary>
+        /// <value>Текущи баланс</value>
 
         public double Balance
         {
             get { return m_balance; }
         }
+        /// <summary>
+        /// Метод снятия средств со счета
+        /// </summary>
+        /// <param name="amount">Сумма списания</param>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// Генерируется, если сумма больше баланса или меньше нуля
+        /// </exception>
 
         public void Debit(double amount)
         {
@@ -42,6 +72,13 @@ namespace BankAccountNS
 
             m_balance += amount;
         }
+        /// <summary>
+        /// Метод зачисления средств на счет
+        /// </summary>
+        /// <param name="amount">Сумма зачисления</param>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// Генерируется, если сумма меньше нуля
+        /// </exception>
 
         public void Credit(double amount)
         {
@@ -52,6 +89,9 @@ namespace BankAccountNS
 
             m_balance += amount;
         }
+        /// <summary>
+        /// Точка входа в программу
+        /// </summary>
 
         public static void Main()
         {
